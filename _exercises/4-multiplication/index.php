30fa25376e2aad0b7c5aeb382451d4d54f155b14
Tahseen Alaa <10079531+TahseenAlaa@users.noreply.github.com>
2018-07-29 01:56:27 +0300

    Multiplication Exercise Solution

--- _exercises/4-multiplication/index.php
+++ _exercises/4-multiplication/index.php
@@ -7,7 +7,57 @@
 	* Write a function that takes 2 values as input and returns the product of them.
 	* For example: multiplication(5,4) => output: 20
 	*/
-   function multiplication()
-  {
-      // CODE HERE
-  }
+
+
+//Solution
+/*** START Multiplication Function ***/
+function multiplication($A, $B) {
+	
+	try {
+		// Validate Input Data
+		if (is_null($A) || is_null($B)) {
+			throw new Exception('Input Should not be Empty!');
+		}elseif (is_string($A) || is_string($B)) {
+			throw new Exception('Input Should be Number!');
+		}elseif (is_array($A) || is_array($B)) {
+			throw new Exception('Input should not be an Array!');
+		}
+		
+		// Stripper Input Data
+		$A = Stripper($A);
+  		$B = Stripper($B);
+  
+		//Retun Result
+		  $result = $A * $B;
+		  if (is_integer($result)) {
+			  echo $result;
+		  }else {
+			throw new Exception('Unknow Error!, Please try again.');
+		  }
+	    }
+			
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
+}
+/*** END Multiplication Function ***/
+
+
+
+/*** START Stripper Function ***/
+function Stripper($data) {
+	//Stripe cSlashes
+	$data = stripcslashes($data);
+	
+	//Stripe PHP and HTML tags
+	$data = strip_tags($data);
+
+	//Stripe Slashes
+	$data = stripslashes($data);
+	return $data;
+}
+/*** END Stripper Function ***/
+
+
+//Final Result
+multiplication(5,5);
